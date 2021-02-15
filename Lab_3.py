3767824adcd7c9f3099192492faaae6b8373f4a1
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-02-15 15:47:46 -0700

    Update Lab_3.py

--- Lab_3.py
+++ Lab_3.py
@@ -26,42 +26,43 @@ GPIO.setup(Eight, GPIO.OUT) #8th LED
 GPIO.setup(Button, GPIO.IN, pull_up_down=GPIP.PUD_DOWN)
 
 count = 0
-while 1:
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(One, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button):
-    count = count + 1
-    GPIO.output(Two, GPIO.HIGH)
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Three, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Four, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Five, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Six, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Seven, GPIO.HIGH) #Turn on LED
-  if GPIO.input(Button): #Button press
-    count = count + 1 #increase count
-    GPIO.output(Eight, GPIO.HIGH) #Turn on LED
-  if count == 8:
-    if GPIO.input(Button): #turn off all LEDs
-      count = 0
-      GPIO.output(One, GPIO.LOW)
-      GPIO.output(Two, GPIO.LOW)
-      GPIO.output(Three, GPIO.LOW)
-      GPIO.output(Four, GPIO.LOW)
-      GPIO.output(Five, GPIO.LOW)
-      GPIO.output(Six, GPIO.LOW)
-      GPIO.output(Seven, GPIO.LOW)
-      GPIO.output(Eight, GPIO.LOW)
+try:
+  while 1:
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(One, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button):
+      count = count + 1
+      GPIO.output(Two, GPIO.HIGH)
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Three, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Four, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Five, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Six, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Seven, GPIO.HIGH) #Turn on LED
+    if GPIO.input(Button): #Button press
+      count = count + 1 #increase count
+      GPIO.output(Eight, GPIO.HIGH) #Turn on LED
+    if count == 8:
+      if GPIO.input(Button): #turn off all LEDs
+        count = 0
+        GPIO.output(One, GPIO.LOW)
+        GPIO.output(Two, GPIO.LOW)
+        GPIO.output(Three, GPIO.LOW)
+        GPIO.output(Four, GPIO.LOW)
+        GPIO.output(Five, GPIO.LOW)
+        GPIO.output(Six, GPIO.LOW)
+        GPIO.output(Seven, GPIO.LOW)
+        GPIO.output(Eight, GPIO.LOW)
 except KeyboardInterupt: #ctrl + C ends loop
   GPIO.cleanup()
     
