40a27960b029c82b07d32a32b4b6994e049706ba
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-02-22 17:44:55 -0700

    Create Lab_4.py
    initial test to light up one LED

---
+++ Lab_4.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+#Set board mode for gpio pins
+GPIO.setmode(GPIO.BCM)
+
+# LED on test
+R1 = 17
+
+GPIO.setmode(R1, GPIO.OUT)
+
+try:
+  while 1:
+    GPIO.output(R1, GPIO.HIGH)
+except KeyboardInterupt:
+  GPIO.cleanup()
