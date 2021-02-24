fbd1a1c206f8ccf1886747e52f06a45a78a56da3
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-02-24 16:37:00 -0700

    Update Lab_4.py
    Added in loop

--- Lab_4.py
+++ Lab_4.py
@@ -5,12 +5,50 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # LED on test
-R1 = 17
+R1 = 4
+R2 = 17
+R3 = 27
+G = 22
+Y1 = 5
+Y2 = 6
+y3 = 13
+#Set difficulty
+easy = 0.5
+normal = 0.25
+hard = 0.1
+diff = easy
 
 GPIO.setup(R1, GPIO.OUT)
+GPIO.setup(R2, GPIO.OUT)
+GPIO.setup(R3, GPIO.OUT)
+GPIO.setup(G, GPIO.OUT)
+GPIO.setup(Y1, GPIO.OUT)
+GPIO.setup(Y2, GPIO.OUT)
+GPIO.setup(Y3, GPIO.OUT)
 
 try:
   while 1:
     GPIO.output(R1, GPIO.HIGH)
-except KeyboardInterupt:
+    time.sleep(diff)
+    GPIO.output(R1, GPIO.LOW)
+    GPIO.output(R2, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(R2, GPIO.LOW)
+    GPIO.output(R3, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(R3, GPIO.LOW)
+    GPIO.output(G, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(G, GPIO.LOW)
+    GPIO.output(Y1, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(Y1, GPIO.LOW)
+    GPIO.output(Y2, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(Y2, GPIO.LOW)
+    GPIO.output(Y3, GPIO.HIGH)
+    time.sleep(diff)
+    GPIO.output(Y3, GPIO.LOW)
+    
+except KeyboardInterrupt:
   GPIO.cleanup()
