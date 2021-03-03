63970925bdfd55149be862d3c300ae3b20452d4b
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-03-02 18:07:32 -0700

    Update ultrasonics.py
    Added other two sensors

--- ultrasonics.py
+++ ultrasonics.py
@@ -6,30 +6,92 @@ import time
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 21
-GPIO_ECHO = 20
+trig1 = 21
+trig2 = 12
+trig3 = 26
+echo1 = 20
+echo2 = 16
+echo3 = 19
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(trig1, GPIO.OUT)
+GPIO.setup(trig2, GPIO.OUT)
+GPIO.setup(trig3, GPIO.OUT)
+GPIO.setup(echo1, GPIO.IN)
+GPIO.setup(echo2, GPIO.IN)
+GPIO.setup(echo3, GPIO.IN)
  
-def distance():
+def distance1():
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    GPIO.output(trig1, True)
  
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(trig1, False)
  
     StartTime = time.time()
     StopTime = time.time()
  
     # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
+    while GPIO.input(echo1) == 0:
         StartTime = time.time()
  
     # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
+    while GPIO.input(echo1) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+def distance2():
+    # set Trigger to HIGH
+    GPIO.output(trig2, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(trig2, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(echo2) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(echo2) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+def distance3():
+    # set Trigger to HIGH
+    GPIO.output(trig3, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(trig3, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(echo3) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(echo3) == 1:
         StopTime = time.time()
  
     # time difference between start and arrival
@@ -43,9 +105,16 @@ def distance():
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
+            dist = distance1()
+            print ("Sensor 1 Distance = %.1f cm" % dist)
             time.sleep(1)
+            dist = distance2()
+            print ("Sensor 2 Distance = %.1f cm" % dist)
+            time.sleep(1)
+            dist = distance3()
+            print ("Sensor 3 Distance = %.1f cm" % dist)
+            time.sleep(1)
+            
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
